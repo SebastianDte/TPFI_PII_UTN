8719a41146a7e505c576d4713e42f63194af8b5a
SebatianDte <sebateumate@outlook.com>
2025-06-18 12:25:51 -0300

    Validación de Cupo en inscripciones

--- include/InscripcionManager.h
+++ include/InscripcionManager.h
@@ -4,19 +4,19 @@
 #include "CursoManager.h"
 #include "AlumnoManager.h"
 #include "Inscripcion.h"
+#include "InscripcionArchivo.h"
 
 class InscripcionManager {
 private:
-	//Aca va luego el atributo NombreArchivo de InscripcionArchivo.
+	InscripcionArchivo _archivoInscripciones;
 	CursoManager _cursoManager;
 	AlumnoManager _alumnoManager;
 	Utilidades _utilidades;
-	
-
 	bool pedirLegajoAlumno(int& legajo);
 	bool pedirIdCurso(int& idCurso);
 	bool pedirImporte(float& importe);
 	bool verificarInscripcionExistente(int legajo, int idCurso);
+
 public:
 	//CRUD Inscripciones
 	void altaInscripcion();
@@ -28,10 +28,12 @@ public:
 	void listarInscripcionesActivas();
 	void listarInscripcionesInactivas();
 	void mostrarUnaInscripcion(const Inscripcion& inscripcion);
-
+	void mostrarInscripcionPorId(int idInscripcion);
 	//Metodo auxiliares.
 	bool existeAlumnoActivo(int legajo);
 	bool existeCursoActivo(int idCurso);
+
+	bool controlCupo(int idCurso);
 
 	void bajaInscripcionesPorCurso(int idCurso);
 	void bajaInscripcionesPorAlumno(int legajo);
--- src/MenuInscripcion.cpp
+++ src/MenuInscripcion.cpp
@@ -1,19 +1,21 @@
 #include "MenuInscripcion.h"
 #include <string>
 #include <iostream>
-
+#include "Utilidades.h"
 void MenuInscripcion::mostrarMenu() {
 	InscripcionManager inscripcionManager;
+	Utilidades _utilidades;
 	int opcion;
 	do {
 		system("cls");
-		std::cout << "=== MENU INSCRIPCION ===\n";
+		_utilidades.limpiarPantallaConEncabezado("MENÚ INSCRIPCION");
 		std::cout << "1. Alta Inscripcion\n";
 		std::cout << "2. Baja Inscripcion\n";
 		std::cout << "3. Modificar Inscripcion\n";
 		std::cout << "4. Listar Inscripciones Activas\n";
 		std::cout << "5. Listar Inscripciones Inactivas\n";
 		std::cout << "6. Listar Todas las Inscripciones\n";
+		std::cout <<  "7. Buscar Inscripcion por ID\n";
 		std::cout << "0. Volver al Menu Principal\n";
 		std::cout << "===========================\n";
 		std::cout << "Ingrese una opcion: ";
@@ -37,6 +39,15 @@ void MenuInscripcion::mostrarMenu() {
 			break;
 		case 6:
 			inscripcionManager.listarTodasInscripciones();
+			break;
+		case 7:
+			int id;
+			std::cout << "Ingrese el ID de la inscripción a buscar: ";
+			std::cin >> id;
+			_utilidades.limpiarPantallaConEncabezado("MENÚ INSCRIPCION");
+			inscripcionManager.mostrarInscripcionPorId(id);
+			break;
+			break;
 		case 0:
 			std::cout << "Volviendo al menu principal...\n";
 			break;
